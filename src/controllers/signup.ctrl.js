00a7aae0a19072060fa6f9b9a4329729007fecc7
Yura Myhaylyak <ghost86421@gmail.com>
2016-07-13 12:48:53 +0300

    Addition signup controller

--- src/controllers/signup.ctrl.js
+++ src/controllers/signup.ctrl.js
@@ -1,6 +1,21 @@
 import app from './../application';
 
 
-app.controller('SignUpCtrl', ($scope, Users, roles) => {
+app.controller('SignUpCtrl', ($scope, Users, roles, $state) => {
 	$scope.rolesList = roles;
+
+	$scope.createUser = (newUser, status) => {
+		if(status) {
+			Users.save({
+				name: newUser.name,
+				email: newUser.email,
+				password: newUser.password,
+				role: newUser.role
+			}, (res) => {
+				if(res.code === 200) {
+					$state.go('home');
+				}
+			});
+		}
+	}
 });
