1e86191b9b39b94424f4571689e40e24d82ad26d
Yura Myhaylyak <ghost86421@gmail.com>
2016-07-22 00:00:45 +0300

    Set translate with temporary fixture data

--- src/config/translate.js
+++ src/config/translate.js
@@ -13,6 +13,11 @@ app.config(($translateProvider) => {
 			SIGN_IN: 'Увійти',
 			SIGN_UP: 'Зареєструватися'
 		},
+		WARNS: {
+			REQUIRED_MESS: 'Це поле обов\'язкове',
+			MIN_LENGTH: ' {{type}} має бути більше ніж {{length}} символи',
+			EMAIL_MESS: 'Це поле має бути email\'ом'
+		},
 		NAVIGATION: {
 			HOME: 'Дім',
 			USERS: 'Користувачі',
@@ -22,7 +27,10 @@ app.config(($translateProvider) => {
 		WRITE_POST: 'Введіть текст для посту',
 		POST: 'Опублікувати',
 		USERS: 'Користувачі',
-		RECENT_POST: 'Нещодавні пости'
+		RECENT_POST: 'Нещодавні пости',
+		POSTED_BY: 'Опубліковано',
+		NAME: 'Ім\'я',
+		PASSWORD: 'Пароль',
 	});
 
 	$translateProvider.preferredLanguage('ua');
