732b5179093aecabce3a6182b257ea0ebf8522f1
Yura Myhaylyak <ghost86421@gmail.com>
2016-07-28 21:13:01 +0300

    remove log of data

--- src/front/controllers/users.ctrl.js
+++ src/front/controllers/users.ctrl.js
@@ -4,7 +4,6 @@ import app from './../application';
 app.controller('UsersCtrl', ($scope, Users) => {
 	$scope.usersPromise = Users.query().$promise.then((data) => {
 		$scope.users = data;
-		console.log($scope.users);
 	}, (err) => {
 		console.error(err);
 	});
