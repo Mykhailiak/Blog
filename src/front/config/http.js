a0d29db08f2d8c5db9989f66f2c3103eb88d4a78
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-05 12:02:24 +0300

    Add new interceptor

--- src/front/config/http.js
+++ src/front/config/http.js
@@ -1,8 +1,12 @@
 import app from './../application';
 
+
 app.config(($httpProvider) => {
 
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
 	$httpProvider.defaults.useXDomain = true;
+
+
+	$httpProvider.interceptors.push('httpRequestInterceptor');
 });
