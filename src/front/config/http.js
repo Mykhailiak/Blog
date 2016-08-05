5f7631d466de0707dd7ce1b38aed3e20ee1effd4
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-05 17:41:39 +0300

    Inject temporary apiKey

--- src/front/config/http.js
+++ src/front/config/http.js
@@ -7,6 +7,8 @@ app.config(($httpProvider) => {
 
 	$httpProvider.defaults.useXDomain = true;
 
+	$httpProvider.defaults.headers.common['apiKey'] = '4ef73a7a69c24adaaa82ae3b595a1f2d4187a7aad6d58ab0c56c6b603d580235'
+
 
 	$httpProvider.interceptors.push('httpRequestInterceptor');
 });
