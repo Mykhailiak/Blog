d5b4a4faa8ee64a873dc91fe4a2e16384527cc6f
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-06 11:22:33 +0300

    Add comment\s submit function

--- src/front/controllers/post.ctrl.js
+++ src/front/controllers/post.ctrl.js
@@ -1,10 +1,27 @@
 import app from './../application';
 
 
-app.controller('PostCtrl', ($scope, Posts, $stateParams, $state) => {
+app.controller('PostCtrl', ($scope, Posts, Comments, $stateParams, $state) => {
 	$scope.postsPromise = Posts.get({id: $stateParams.id}).$promise.then((data) => {
 		$scope.post = data;
+		console.log(data);
 	}).catch((err) => {
 		console.error(err);
 	});
+
+	$scope.comment = {};
+
+	$scope.postComment = (data) => {
+		let newComment = {
+			post_for_comment: $scope.post.id,
+			comment_name: data.title,
+			comment_text: data.text
+		};
+
+		Comments.save(newComment).then((comment) => {
+			$scope.post.comment.push(comment);
+		}).catch((err) => {
+			console.err(err);
+		});
+	};
 });
