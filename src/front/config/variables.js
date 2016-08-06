6ca3c1239deb73c09ec073463eb1a85a95a807af
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-06 12:39:52 +0300

    Change prop

--- src/front/config/variables.js
+++ src/front/config/variables.js
@@ -6,7 +6,7 @@ import mapData from './../../libs/map.json';
 import loadTpl from './../../themes/templates/preloader_tpls/preloadPromise.tpl.html';
 
 app.constant('backEndUrl', data.backEndUr);
-app.value('roles', data.roles);
+app.value('roles', data.ROLES);
 app.value('mapConst', mapData);
 app.value('cgBusyDefaults', {
 	templateUrl: loadTpl,
