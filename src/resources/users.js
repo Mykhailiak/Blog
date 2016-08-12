77cff019add396a25d1f17808877e0694e2d733d
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-12 19:43:41 +0300

    Add update method

--- src/resources/users.js
+++ src/resources/users.js
@@ -3,5 +3,9 @@ import module from './module';
 module.factory('Users', ($resource, backEndUrl) => {
 	return $resource(`${backEndUrl}/user/:id`, {
 		id: '@id'
+	}, {
+		update: {
+			method: 'PUT'
+		}
 	});
 });
