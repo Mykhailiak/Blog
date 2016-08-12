bb141efdd8d0d76d75811d0b3e48c90cbbeb0a7a
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-12 13:47:00 +0300

    Define auth functionality

--- src/front/controllers/signin.ctrl.js
+++ src/front/controllers/signin.ctrl.js
@@ -1,8 +1,19 @@
 import app from './../application';
 
 
-app.controller('SignInCtrl', ($scope) => {
-	$scope.authUser = (user) => {
-		
-	}
+app.controller('SignInCtrl', ($scope, $http, backEndUrl, $rootScope) => {
+	$scope.authUser = (user, status) => {
+		if(status) {
+			$http.post(`${backEndUrl}/authorization`, {
+				user_name: user.name,
+				user_email: user.email,
+				user_password: user.password
+			}).then((data) => {
+				$rootScope.authUser = data;
+				localStorage.setItem('apiKey', data.apiKey);
+			}).catch((err) => {
+				console.error(err);
+			});
+		}
+	};
 });
