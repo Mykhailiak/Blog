01007ed751fcaf3c682ada988b54555f38582c26
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-12 20:21:25 +0300

    Add basis of update user

--- src/front/controllers/account.sett.ctrl.js
+++ src/front/controllers/account.sett.ctrl.js
@@ -1,6 +1,30 @@
 import app from './../application';
 
 
-app.controller('AccountSettCtrl', ($scope) => {
+app.controller('AccountSettCtrl', ($scope, Users, $state, $stateParams) => {
 	$scope.title = 'Hello';
+
+	Users.get({id: $stateParams.id}).$promise.then((user) => {
+		$scope.settings = {
+			name: user.username,
+			email: user.email,
+			password: user.password
+		}
+	});
+
+	$scope.updateUser = (data, status) => {
+		if(status) {
+			Users.update({id: $stateParams.id}, {
+				user_name: data.name,
+				user_email: data.email,
+				user_password: data.password
+			}).$promise.then((user) => {
+				$rootScope.authUser = user;
+				$state.go('^.home');
+			}).catch((err) => {
+				console.error(err);
+			});
+		}
+	};
+
 });
