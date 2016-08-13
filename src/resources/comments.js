73e722b7059478968bf42858b59c3251e4f1f8c7
Yura Myhaylyak <ghost86421@gmail.com>
2016-08-13 18:40:35 +0300

    Add update functionality to comment

--- src/resources/comments.js
+++ src/resources/comments.js
@@ -3,5 +3,9 @@ import module from './module';
 module.factory('Comments', ($resource, backEndUrl) => {
 	return $resource(`${backEndUrl}/comment/:id`, {
 		id: '@id'
+	}, {
+		update: {
+			method: 'PUT'
+		}
 	});
 });
