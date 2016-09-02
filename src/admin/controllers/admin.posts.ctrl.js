7d12a3a89925e4af3dfdc0f5e55d7f27f4db1cad
Yura Myhaylyak <ghost86421@gmail.com>
2016-09-02 23:39:00 +0300

    Admin post list

--- src/admin/controllers/admin.posts.ctrl.js
+++ src/admin/controllers/admin.posts.ctrl.js
@@ -7,4 +7,12 @@ admin.controller('AdminPostsCtrl', ($scope, Posts) => {
 	}).catch((err) => {
 		console.error('Posts: ', err);
 	});
+
+	$scope.deletePost = (post, id) => {
+		Posts.delete({id: id}).$promise.then((data) => {
+			$scope.posts.splice($scope.posts.indexOf(post), 1);
+		}).catch((err) => {
+			console.error('Delete post', err);
+		});
+	};
 });
