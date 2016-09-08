1da650110171191c3109b9ae732fd134b1557a08
Yura Myhaylyak <ghost86421@gmail.com>
2016-09-08 19:32:27 +0300

    Add edit post

--- src/admin/controllers/admin.post.edit.ctrl.js
+++ src/admin/controllers/admin.post.edit.ctrl.js
@@ -1,6 +1,22 @@
 import admin from './../admin';
 
 
-admin.controller('AdminPostEditCtrl', ($scope) => {
+admin.controller('AdminPostEditCtrl', ($scope, $state, $stateParams, Posts) => {
 	$scope.title = 'Post edit tpl';
+
+	$scope.postPromise = Posts.get({id: $stateParams.id}).$promise.then((post) => {
+		console.log(post);
+		$scope.post = post;
+	}).catch((err) => {
+		console.error('Post error: ', err);
+	});
+
+	$scope.editPost = (post) => {
+		Posts.update({id: $stateParams.id}, post).$promise.then((post) => {
+			console.info('Update success');
+			$state.go('admin.posts');
+		}).catch((err) => {
+			console.error('Post update', err);
+		});
+	};
 });
